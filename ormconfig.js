9eaf1bc58c0d13439fc017e2f5549ae1eaf86f3c
PlasticBucket1944 <Orangina2739@gmail.com>
2022-09-26 15:51:40 +0900

    debug

--- ormconfig.js
+++ ormconfig.js
@@ -1,5 +1,6 @@
 module.exports = {
-  type: process.env.DB_TYPE,
+  // type: process.env.DB_TYPE,
+  type: 'postgres',
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   username: process.env.DB_USERNAME,
