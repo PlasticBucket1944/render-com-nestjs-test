43d821af250ce4b29c1671576c75e6c8cbfbb269
PlasticBucket1944 <Orangina2739@gmail.com>
2022-09-26 12:09:18 +0900

    ポート設定をテスト

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  console.log(process.env.PORT);
   const port = process.env.PORT || 3000;
-  await app.listen(3000, '0.0.0.0');
+  await app.listen(port, '0.0.0.0');
 }
 bootstrap();
