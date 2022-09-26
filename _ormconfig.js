8307f2f579ec1fb43d128eb7550a87deae8a395f
PlasticBucket1944 <Orangina2739@gmail.com>
2022-09-26 17:39:37 +0900

    DBG

--- _ormconfig.js
+++ _ormconfig.js
@@ -1,6 +1,5 @@
 module.exports = {
-  // type: process.env.DB_TYPE,
-  type: 'postgres',
+  type: process.env.DB_TYPE,
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   username: process.env.DB_USERNAME,
--- src/app.module.ts
+++ src/app.module.ts
@@ -5,7 +5,21 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot(),
+    TypeOrmModule.forRoot({
+        type: 'postgres',
+        host: process.env.DB_HOST,
+        port: Number(process.env.DB_PORT),
+        username: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_DATABASE,
+        autoLoadEntities: true,
+        entities: ['dist/entities/*.entity.js'],
+        migrations: ['dist/migrations/*.js'],
+        cli: {
+            entitiesDir: 'src/entities',
+            migrationsDir: 'src/migrations',
+        },
+      }),
   ],
   controllers: [AppController],
   providers: [AppService],
