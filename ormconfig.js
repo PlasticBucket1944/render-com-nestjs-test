26eca51c804e340b20b4ab4963efe9881f87be12
PlasticBucket1944 <Orangina2739@gmail.com>
2022-09-26 14:45:54 +0900

    db設定追加

---
+++ ormconfig.js
@@ -0,0 +1,15 @@
+module.exports = {
+  type: process.env.DB_TYPE,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  autoLoadEntities: true,
+  entities: ['dist/entities/*.entity.js'],
+  migrations: ['dist/migrations/*.js'],
+  cli: {
+      entitiesDir: 'src/entities',
+      migrationsDir: 'src/migrations',
+  },
+};
