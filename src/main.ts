17fbdbfdeb9ffeec2aaf4205db584e78c54f1515
PlasticBucket1944 <Orangina2739@gmail.com>
2022-09-26 23:56:59 +0900

    test

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   console.log(process.env.PORT);
   console.log(process.env.HOGE || 'HOGE IS NOTTING');
+  console.log(__dirname);
   const port = process.env.PORT || 3000;
   await app.listen(port, '0.0.0.0');
 }
